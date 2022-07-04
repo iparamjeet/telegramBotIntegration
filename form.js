87086df1803250dff67178bc2cb69be4ec811883
iParamjeet <paramjeetyadav94@gmail.com>
2022-07-04 18:25:29 +0530

    Param: small modification in fields

--- form.js
+++ form.js
@@ -6,17 +6,22 @@ form.addEventListener("submit", function(e){
     var text1 = document.getElementById("text1").value;
     var text2 = document.getElementById("text2").value;
 
-    var msg = `Result is : %0A - Text1 : ${text1} %0A -Text2: ${text2}`
+   // var msg = `Result is : %0A - Text1 : ${text1} %0A -Text2: ${text2}`
+
+   var msg = text1 + text2;
 
     var token = "5584784948:AAGHCvNBrwTiTx-VoJcSzjVKR2fpHzgHqGY";
     var chat_id = -658036982
 
     var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${msg}`
 
+    console.log(url);
+
     let api = new XMLHttpRequest();
     api.open("GET", url, true);
     api.send();
 
     form.reset();
-    console.log(msg);
+   // console.log(msg);
+    
 })
